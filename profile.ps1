8d6554ecad2bd98ad3ad6c3d91fd68e9b5d010a3
Russel <59341541+moodyKroko@users.noreply.github.com>
2023-10-21 14:46:04 +0100

    delete: redundant file

--- profile.ps1
+++
@@ -1,8 +0,0 @@
-function Pro {notepad $PROFILE.CurrentUserAllHosts}
-function Get-CmdletAlias ($cmdletname) {
-  Get-Alias |
-    Where-Object -FilterScript {$_.Definition -like "$cmdletname"} |
-      Format-Table -Property Definition, Name -AutoSize
-}
-
-Set-Alias -Name vim -Value C:\tools\neovim\Neovim\bin\nvim.exe
