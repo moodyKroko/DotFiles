a2a4b193c96b10dd6f59ac9dbec06bc0574f8df3
Russel <59341541+moodyKroko@users.noreply.github.com>
2024-03-25 15:38:45 +0000

    change colorscheme to one_monokai

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,14 +1,41 @@
 return {
+  -- current color scheme
   {
-    "catppuccin/nvim",
-    name = "catppuccin",
-    enabled = false,
+    "cpea2506/one_monokai.nvim",
+    lazy = false,
+    -- enabled = false,
+    priority = 1000,
+    opts = function()
+      return {
+        transparent = true,
+
+        -- https://coolors.co/f6c173-703aa6-6d58a7-6ba292-acadbc
+        themes = function(colors)
+          return {
+            border = { fg = colors.purple, bg = colors.none },
+            FloatBorder = { fg = colors.purple, bg = colors.none },
+
+            LineNr = { fg = colors.fg:darken(0.5) },
+            ColorColumn = { bg = colors.purple:darken(0.3) },
+            CursorLine = { bg = colors.none },
+            CursorLineNr = { fg = colors.fg },
+            Search = { fg = colors.black, bg = colors.red },
+
+            CmpPmenu = { bg = colors.none },
+            CmpSel = { fg = colors.white, bg = colors.fg:darken(0.5) },
+            CmpDoc = { bg = colors.none },
+
+            DashboardHeader = { fg = colors.aqua },
+          }
+        end,
+      }
+    end,
   },
 
-  -- current color scheme
   {
     "rose-pine/neovim",
     lazy = false,
+    enabled = false,
     name = "rose-pine",
     priority = 1000,
     config = function()
@@ -26,11 +53,13 @@ return {
           groups = {
             border = "#f6c173", --"pine",
             -- background = "none",
+            -- TODO: make the green foreground color a lil darker
           },
 
           highlight_groups = {
             ColorColumn = { bg = "highlight_low", blend = 40 },
-            CursorLine = { bg = "highlight_med", blend = 90 },
+            -- CursorLine = { bg = "none", blend = 90 },
+            CursorLine = { bg = "none" },
 
             Search = { bg = "love", inherit = false },
             FloatBorder = { fg = "#f6c173", bg = "none" },
